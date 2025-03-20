db3de4d33ee3f457b13b544ec1bd4613fdcda89b
Avishka Madushanka <avishka.madushanka@qualitapps.com>
2025-03-20 20:05:27 +0530

    index routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 
@@ -57,16 +58,26 @@ $tasks = [
 ];
 
 
+Route::get('/', function () {
+    return redirect() -> route('tasks.index');
+});
 
 
-Route::get('/', function () use($tasks) {
+Route::get('/tasks', function () use($tasks) {
     return view('index' , [
           'tasks' => $tasks,
     ] );
 }) -> name('tasks.index');
 
-Route::get( '/{id}' , function($id){
-    if($id == 1) return 'this is one';
-    return 'this is other';
+Route::get( '/tasks/{id}' , function($id) use($tasks){
+   $task = collect($tasks) -> firstWhere('id', $id);
+
+   if(!$task) {
+    abort(Response::HTTP_NOT_FOUND);
+   }
+
+   return view('show', [
+    'task'=> $task
+   ]);
 }) ->name('tasks.show');
 
