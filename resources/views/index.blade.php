cf012dc5e999ae2244ae1f71f349bc9988643032
Avishka Madushanka <avishka.madushanka@qualitapps.com>
2025-03-21 11:49:40 +0530

    Layour config

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,27 @@
-<h1>Task Lists</h1>
+@extends('layouts.app')
+
 <div>
     @if(Count($tasks))
         @foreach ($tasks as $task)
 
             <div>
-                <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
-                    <h2>{{ $task->title }}</h2>
-                </a>
+                @section('title')
+                    <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                        <h2>{{ $task->title }}</h2>
+                    </a>
+                @endsection
                 <br>
-                <p>{{  $task->description }}</p>
-                <p>{{ $task->long_description }}</p>
-                <div class="dates">
-                    @if($task->completed)
-                        <p>Completed</p>
-                    @else
-                        <p>-----</p>
-                    @endif
-                </div>
+                @section('section')
+                    <p>{{  $task->description }}</p>
+                    <p>{{ $task->long_description }}</p>
+                    <div class="dates">
+                        @if($task->completed)
+                            <p>Completed</p>
+                        @else
+                            <p>-----</p>
+                        @endif
+                    </div>
+                @endsection
             </div>
 
         @endforeach
