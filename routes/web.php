afb5272e5d1dca3d0f621ff11cf25f8d01c15926
Avishka Madushanka <avishka.madushanka@qualitapps.com>
2025-03-25 11:23:15 +0530

    storing task data in database

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 // use illuminate\Http\Response;
 use Illuminate\Http\Request;
+use \App\Models\Task;
 // use Illuminate\Mail\Mailables\Headers;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
@@ -31,5 +32,17 @@ Route::get('/tasks/{id}', function ($id) {
 })->name('tasks.show');
 
 Route::post('/tasks', function (Request $request) {
-    dd($request->all());
+    $data = $request->validate([
+        'title'=> 'required|max:255',
+        'description'=> 'required',
+        'long_description'=> 'required',
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', [ 'id'=> $task->id])->with('success','succsesfully created');
 })->name('tasks.store');
