3aaee80b86bdd4a3b9bced32517471e7601e0fe6
Avishka Madushanka <avishka.madushanka@qualitapps.com>
2025-03-25 12:50:42 +0530

    form validations

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -8,14 +8,23 @@
         <div>
             <label for="title">Title</label>
             <input type="text" name="title" id="title">
+            @error('title')
+            <p>Please enter the title</p>
+            @enderror
         </div>
         <div>
             <label for="description">Description</label>
             <textarea name="description" id="description" rows="5"></textarea>
+            @error('description')
+            <p>Please enter the description</p>
+            @enderror
         </div>
         <div>
             <label for="long_description">Long Description</label>
             <textarea name="long_description" id="long_description" rows="10"></textarea>
+            @error('long_description')
+            <p>Please enter the long description</p>
+            @enderror
         </div>
         <div>
             <button type="submit">Add Task</button>
